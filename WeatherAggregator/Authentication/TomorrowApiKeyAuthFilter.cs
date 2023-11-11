b79a09e49bacb1931a33e39b4786610e6f12e117
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-11-11 10:45:50 +0500

    Authentication for different keys

---
+++ WeatherAggregator/Authentication/TomorrowApiKeyAuthFilter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WeatherAggregator.Authentication
+{
+	public class TomorrowApiKeyAuthFilter : IAuthorizationFilter
+	{
+		private readonly IConfiguration _configuration;
+
+		public TomorrowApiKeyAuthFilter(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public void OnAuthorization(AuthorizationFilterContext context)
+		{
+			if (!context.HttpContext.Request.Headers.TryGetValue(AuthConstants.TApiKeyHeaderName, out
+					var extractedApiKey))
+			{
+				context.Result = new UnauthorizedObjectResult("Api Key was not provided");
+				return;
+			}
+			var apiKey = _configuration.GetValue<string>(AuthConstants.TApiKeySectionName);
+			if (!apiKey.Equals(extractedApiKey))
+			{
+				context.Result = new UnauthorizedObjectResult("Invalid Api Key");
+				return;
+			}
+		}
+	}
+}
+
---
+++ WeatherAggregator/Authentication/WeatherApiKeyAuthFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherAggregator.Authentication
+{
+	public class WeatherApiKeyAuthFilter : IAuthorizationFilter
+	{
+		private readonly IConfiguration _configuration;
+
+		public WeatherApiKeyAuthFilter(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public void OnAuthorization(AuthorizationFilterContext context)
+		{
+			if (!context.HttpContext.Request.Headers.TryGetValue(AuthConstants.WApiKeyHeaderName, out
+					var extractedApiKey))
+			{
+				context.Result = new UnauthorizedObjectResult("Api Key was not provided");
+				return;
+			}
+			var apiKey = _configuration.GetValue<string>(AuthConstants.WApiKeySectionName);
+			if (!apiKey.Equals(extractedApiKey))
+			{
+				context.Result = new UnauthorizedObjectResult("Invalid Api Key");
+				return;
+			}
+		}
+	}
+}
