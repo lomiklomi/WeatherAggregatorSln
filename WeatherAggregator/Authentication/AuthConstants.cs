88321b881460d0f9d95c249f3700a07d36f564f4
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-11-11 10:45:06 +0500

    Added W-Api-Key and T-Api-Key

--- WeatherAggregator/Authentication/AuthConstants.cs
+++ WeatherAggregator/Authentication/AuthConstants.cs
@@ -4,5 +4,11 @@
 	{
 		public const string ApiKeySectionName = "Authentication:ApiKey";
 		public const string ApiKeyHeaderName = "X-Api-Key";
+
+		public const string WApiKeySectionName = "Authentication:WApiKey";
+		public const string WApiKeyHeaderName = "W-Api-Key";
+
+		public const string TApiKeySectionName = "Authentication:TApiKey";
+		public const string TApiKeyHeaderName = "T-Api-Key";
 	}
 }
