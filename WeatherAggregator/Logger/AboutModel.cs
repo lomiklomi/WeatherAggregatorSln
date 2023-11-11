22a5c97f83048b2a687bb81e81e7678a92fabf72
lomiklomi <55328668+lomiklomi@users.noreply.github.com>
2023-11-11 10:47:31 +0500

    Model logger

---
+++ WeatherAggregator/Logger/AboutModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherAggregator.Logger
+{
+	public class AboutModel : PageModel
+	{
+		private readonly ILogger _logger;
+
+		public AboutModel(ILogger<AboutModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+			_logger.LogInformation("About page visited at {DT}",
+				DateTime.UtcNow.ToLongTimeString());
+		}
+	}
+}
